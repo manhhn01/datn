b55fb33517267e483563ec0a06694d9d31f81336
Nguyen Manh <manhhn01@gmail.com>
2023-11-19 21:25:41 +0700

    fix(api): load app config

--- packages/api/src/modules/app/app.module.ts
+++ packages/api/src/modules/app/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from '@config/app';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+      load: [appConfig]
     }),
   ],
   controllers: [AppController],
